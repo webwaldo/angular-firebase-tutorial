357523e9aafe3ad04b8ecd65caf71b008d6ba6a8
Sam Brook <brookyy@gmail.com>
2014-07-04 08:19:10 +1000

    Adding to a list of nodes via the on method

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,46 +5,30 @@ angular.module('firebaseApp')
   .controller('MainCtrl', function ($scope, $timeout) {
 
     var rootRef = new Firebase('https://brookyy.firebaseio.com/');
-    var childRef = rootRef.child('message');
-    console.log('root ref', rootRef);
-    childRef.on('value', function(snapshot) {
+    var messagesRef = rootRef.child('messages');
+
+    $scope.currentUser = null;
+    $scope.currentText = null;
+
+
+
+    messagesRef.on('value', function(snapshot) {
       $timeout( function() {
+        console.log(messagesRef);
         var snapshotVal = snapshot.val();
         console.log(snapshotVal);
-        snapshot.forEach( function(item){
-          console.log( item.name() + ' - ' + item.val() );
-          console.log(item.ref());
-        });
-        $scope.message = snapshotVal;
+        $scope.messages = snapshotVal;
       });
     });
 
-    $scope.$watch('message.text', function(newVal){
-      console.log(newVal);
-      if( !newVal){
-        return;
-      }
-      childRef.update({
-        text: newVal
-      });
-    });
+    $scope.sendMessage = function(){
+      console.log('send dat message');
+      var newMessage = {
+        user: $scope.currentUser,
+        text: $scope.currentText
+      };
 
-    // https://brookyy.firebaseio.com/message
-    $scope.setMessage = function(){
-      childRef.set({
-        user: 'Bob',
-        text: 'Hi'
-      });
-    };
-
-    $scope.updateMessage = function() {
-      childRef.update({
-        moo: 'Smith two'
-      });
-    };
-
-    $scope.deleteMessage = function() {
-      childRef.remove();
+      messagesRef.push(newMessage);
     };
 
   });
