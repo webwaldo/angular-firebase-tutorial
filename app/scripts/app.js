18500a2a2ac2917f53374044728c2e73b167ca09
Sam Brook <brookyy@gmail.com>
2014-07-07 08:34:57 +1000

    Setup views and controllers and routing  for login,register and chat

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,18 @@ angular.module('firebaseApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/chat',{
+        templateUrl: 'views/chat.html',
+        controller: 'ChatCtrl'
+      })
+      .when('/login',{
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/register',{
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
