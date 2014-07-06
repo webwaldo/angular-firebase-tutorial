8cce14a4fce86cf4b6dd091914a41dc46c8d9092
Sam Brook <brookyy@gmail.com>
2014-07-06 14:26:32 +1000

    Adding angularfire module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('firebaseApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'firebase'
 ])
   .config(function ($routeProvider) {
     $routeProvider
