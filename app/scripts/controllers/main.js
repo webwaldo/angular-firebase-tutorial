b212b200455ed5f937b44cfb3fcd12aff4f94cc0
Sam Brook <brookyy@gmail.com>
2014-07-11 07:55:46 +1000

    testing the auth rules in FB

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,46 +1,9 @@
 'use strict';
 
 angular.module('firebaseApp')
-  .controller('MainCtrl', function ($scope, $timeout, MessageService) {
-
-    $scope.currentUser = null;
-    $scope.currentText = null;
-    $scope.messages = [];
-
-
-    MessageService.childAdded(function(addedChild){
-        $scope.messages.push(addedChild);
-    });
-
-
-    $scope.sendMessage = function(){
-      console.log('send dat message');
-      var newMessage = {
-        user: $scope.currentUser,
-        text: $scope.currentText
-      };
-      var promise = MessageService.add(newMessage);
-      promise.then( function(data){
-        console.log(data.name());
-      });
-    };
-
-    $scope.turnFeedOff = function(){
-      MessageService.off();
-    };
-
-    $scope.pageNext = function(){
-      var lastItem = $scope.messages[$scope.messages.length - 1];
-      MessageService.pageNext(lastItem.name, 10).then( function(messages){
-        $scope.messages = messages;
-      });
-    };
-
-    $scope.pageBack = function(){
-      var firstItem = $scope.messages[0];
-      MessageService.pageBack(firstItem.name, 10).then( function(messages){
-        $scope.messages = messages;
-      });
-    };
+  .controller('MainCtrl', function ($scope, FBURL) {
+    var fbRef = new Firebase(FBURL).child('secret');
+    
+    fbRef.set(444);
 
   });
